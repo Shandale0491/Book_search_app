4747d8c411272e5afa58d47c8827f83212447189
Shandale Shelton <Dale0491@gmail.com>
2018-11-16 08:05:35 -0600

    Add files via upload

---
+++ book_search/Book_search_app/README.md
@@ -0,0 +1,3 @@
+# Book_search_app
+Book search app using google books API 
+In this application the technoligies that are used is HTML, materialize css and JavaScript for the UI. For the API simple AJAX call was used to get the app up and running. 
---
+++ book_search/assets/js/app.js
@@ -0,0 +1,46 @@
+function bookSearch(event) {
+  let search = document.getElementById('book-input').value;
+  let apiKey = '&key=AIzaSyBXbAOQnU8C6Cg_0FNdhvYMM';
+  document.getElementById('results').innerHTML = '';
+
+  $.ajax({
+    type: 'GET',
+    url: 'https://www.googleapis.com/books/v1/volumes?q=' + search,
+    apiKey,
+    dataType: 'json',
+
+    success: function(data) {
+      console.log(data);
+      let moreInfo = 'More Info';
+      for (i = 0; i < data.items.length; i++) {
+        results.innerHTML += '<h3>' + data.items[i].volumeInfo.title + '</h3>';
+        results.innerHTML +=
+          '<h5>' + data.items[i].volumeInfo.authors + '</h5>';
+        results.innerHTML +=
+          '<h6>' + data.items[i].volumeInfo.publisher + '</h6>';
+        results.innerHTML +=
+          '<img src =' + data.items[i].volumeInfo.imageLinks.thumbnail + '/>';
+        results.innerHTML +=
+          '<a href=' +
+          data.items[i].volumeInfo.infoLink +
+          ' target="blank_">' +
+          moreInfo +
+          '</a>';
+      }
+    },
+    error: function(err) {
+      alert('Error');
+    }
+  });
+}
+
+document.getElementById('button').addEventListener('click', bookSearch, false);
+
+let input = document.getElementById('book-input');
+input.addEventListener('keydown', function(event) {
+  const enterKeyIsDown = event.keyCode === 13;
+  if (enterKeyIsDown) {
+    event.preventDefault();
+    document.getElementById('button').click();
+  }
+});
